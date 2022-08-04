ae76b03fce6ff47389577722a300c94abb939b7a
Muhammad Algshy <muhammad.algshy@wfp.org>
2022-08-04 13:07:31 +0200

    Creat email

--- allauth_tmp/apps/accounts/adapter.py
+++ allauth_tmp/apps/accounts/adapter.py
@@ -1,8 +1,8 @@
 from django.utils.translation import gettext_lazy as _
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.account.adapter import get_adapter as get_account_adapter
 from .models import User
+from allauth.socialaccount.models import EmailAddress
 from django.core.exceptions import ValidationError
 from django.conf import settings
 class MyCustomAdapter(DefaultAccountAdapter):
@@ -31,11 +31,11 @@ class MyCustomSocialAdapter(DefaultSocialAccountAdapter):
 
         def pre_social_login(self, request, sociallogin):
 
-            # social account already exists, so this is just a login
+            # # social account already exists, so this is just a login
             if sociallogin.is_existing:
                 return
 
-            # some social logins don't have an email address
+            # # some social logins don't have an email address
             if not sociallogin.email_addresses:
                 return
 
@@ -52,9 +52,22 @@ class MyCustomSocialAdapter(DefaultSocialAccountAdapter):
 
             try:
                 existing_user = User.objects.get(email__iexact=email.email)
+                existing_user.first_name=sociallogin.user.first_name
+                existing_user.last_name=sociallogin.user.last_name
+                existing_user.save()
             except User.DoesNotExist:
                 raise ValidationError(self.error_messages["no_user"])
 
+            # Creat email Addresses 
+            if  sociallogin.email_addresses:
+                new_email=EmailAddress(
+                    user=existing_user,
+                    email=sociallogin.account.extra_data['email'],
+                    verified=sociallogin.account.extra_data['verified_email'],
+                    primary=True
+                )
+                new_email.save()
+            # Connect existing user to the logined social account
             sociallogin.connect(request, existing_user)
     else:
         pass
